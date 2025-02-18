18aa2fa0510ef192cce5ac1f4c9057260679fc16
RahulMikaelson <mikaelsonrahul@gmail.com>
2025-02-19 04:00:04 +0530

    feat: Add service registry using Spring Cloud Netflix Eureka

---
+++ backend/service-registry/src/test/java/com/elite/service_registry/ServiceRegistryTests.java
@@ -0,0 +1,13 @@
+package com.elite.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
